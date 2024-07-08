324966ce79b9a8def03a3ada0d8006530a870cb6
Pantamma <chsurya2325@gmail.com>
2024-07-09 01:48:33 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,40 @@
+function navigateTo(page) {
+    window.location.href = page + '.html';
+}
+
+function openForm() {
+    document.getElementById('form-modal').style.display = 'block';
+}
+
+function closeForm() {
+    document.getElementById('form-modal').style.display = 'none';
+}
+
+function editStudent(id) {
+    // Logic for editing a student
+    alert('Edit student with ID ' + id);
+}
+
+function deleteStudent(id) {
+    // Logic for deleting a student
+    alert('Delete student with ID ' + id);
+}
+
+function editCourse(id) {
+    // Logic for editing a course
+    alert('Edit course with ID ' + id);
+}
+
+function deleteCourse(id) {
+    // Logic for deleting a course
+    alert('Delete course with ID ' + id);
+}
+
+function navigateTo(page) {
+    window.location.href = page + '.html';
+}
+
+
+
+
+
